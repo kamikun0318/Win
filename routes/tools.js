e5fe2e252ab26f39f857600a318915b2a982096b
Toka_Kun_ <222786419+toka-kun@users.noreply.github.com>
2026-06-09 17:57:19 +0900

    Add route for embedded tools page

--- routes/tools.js
+++ routes/tools.js
@@ -7,6 +7,11 @@ router.get("/", (req, res) => {
   res.render("tools/home");
 });
 
+// --- 埋め込み画面 ---
+router.get("/emb", (req, res) => {
+  res.render("tools/emb");
+});
+
 // tool
 router.get("/tool/:id", (req, res) => {
   res.render(`tools/tool/${req.params.id}`);
